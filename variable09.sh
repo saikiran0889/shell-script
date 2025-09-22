03c1a6ab91ecc73d972e49792e15c3ff7310cf7e
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-09-22 13:30:03 +0530

    var

--- variable09.sh
+++ variable09.sh
@@ -4,7 +4,7 @@
 trump=$us
 modi=$in
 
-echo "$us:hi $in"
-echo "$in:hi $us how r u?"
-echo "$us:i am good, how r u $in"
-echo "$in: i am good, thanks"
+echo "$us::hi $in"
+echo "$in::hi $us how r u?"
+echo "$us::i am good, how r u $in"
+echo "$in:: i am good, thanks"
