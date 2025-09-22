93fedd5bf14737419446d57a0aa2eb4dd279c1bd
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-09-22 22:49:52 +0530

    var

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "ERROR: You are not in root access"
 fi
 
 dnf install mysql -y
-if [ $? -ne 0];then
+if [ $? -ne 0 ];then
 echo "error while installing"
     else
 echo "succussfully installed"
