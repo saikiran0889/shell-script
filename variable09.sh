3e719f9aa11ecfba780fb17f250bf5998dc6ce1c
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-09-22 13:41:50 +0530

    var

--- variable09.sh
+++ variable09.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-person1=$tump
-person2=$modi
+person1=$1
+person2=$2
 
 echo "$person1:: hi $person2"
 echo "$person2:: hi $person1 how r u?"
 echo "$person1:: i am good, how r u $person2"
-echo "$person2:: i am good, thankss"
+echo "$person2:: i am good, thanksq"
