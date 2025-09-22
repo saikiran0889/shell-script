f043da164478114d3517d78cb45b13426a8dacf3
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-09-22 16:18:04 +0530

    var

--- specialvar.sh
+++ specialvar.sh
@@ -4,6 +4,7 @@ echo "print all vaiables in linux: $@"
 
 echo "print all vaiables in linux: $*"
 echo "script name: $0"
-echo "current working diectory: $PWD"
+echo "current working diectory: $pwd"
 echo "who is running the scipt: $USER"
 echo "Home die=rectory: $HOME"
+echo "PID of this script: $$"
