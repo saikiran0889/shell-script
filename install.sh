8150fe7b3652009f1e1584a3d5566a1d62da65b1
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-09-22 22:52:20 +0530

    var

--- install.sh
+++ install.sh
@@ -4,11 +4,13 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
 echo "ERROR: You are not in root access"
+exit 1
 fi
 
 dnf install mysql -y
 if [ $? -ne 0 ];then
 echo "error while installing"
+exit 1
     else
 echo "succussfully installed"
 fi
