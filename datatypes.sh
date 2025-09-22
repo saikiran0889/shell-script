834437f2f3682eca0511b7605b9b943026bf00d0
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-09-22 16:56:41 +0530

    var

--- datatypes.sh
+++ datatypes.sh
@@ -2,8 +2,11 @@
 
 num1=100
 num2=200
-name=devops
+NAME=devops
 
-sum=$(($num1+$num2+$name))
+sum=$(($num1+$num2+$NAME))
 
 echo "total sum : ${sum}"
+
+LEADERS=("modi" "putin" "trunp")
+echo "leaders names= ${LEADERS@}"
