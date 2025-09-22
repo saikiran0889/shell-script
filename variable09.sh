fffd87c87e24b9eb0032f226c94087af9428522a
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-09-22 13:34:37 +0530

    var

--- variable09.sh
+++ variable09.sh
@@ -4,7 +4,7 @@
 us=$tump
 in=$modi
 
-echo "$us::hi $in"
-echo "$in::hi $us how r u?"
-echo "$us::i am good, how r u $in"
+echo "$us:: hi $in"
+echo "$in:: hi $us how r u?"
+echo "$us:: i am good, how r u $in"
 echo "$in:: i am good, thanks"
