f7f0bb0653090a390bc75a6ca6bda24d2ce73479
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-09-23 13:32:23 +0530

    install

--- colourinstal.sh
+++ colourinstal.sh
@@ -4,6 +4,7 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
 if [ $USERID -ne 0 ]; then
 echo -e "$R ERROR: You are not in root access $Y"
 exit 1
@@ -15,7 +16,7 @@ validate(){
 echo "error while installing $2"
 exit 1
     else
-echo -e "$Y succussfully $2 installed $G"
+echo -e "$Y succussfully $2 installed $N"
 fi
 }
 
