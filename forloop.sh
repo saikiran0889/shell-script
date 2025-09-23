8233032687b0de24c1f0d2569e9b913f9a96b020
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-09-23 21:50:27 +0530

    install

--- forloop.sh
+++ forloop.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
-for i in {1..100}
-do
-echo "Print: $i"
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+logsfolder="/var/log/shell-script.txt"
+scriptname=$( echo $0 | cut -d "." -f1 )
+logfile="$logsfolder/$scriptname.log"
+
+mkdir -p $logsfolder
+
+echo "script started executed : $(date)" | tee -a $logfile
+
+
+if [ $USERID -ne 0 ]; then
+echo -e "$R ERROR: You are not in root access $Y"
+exit 1
+fi
+validate(){
+
+    if [ $1 -ne 0 ];then
+echo "error while installing $2"
+exit 1
+    else
+echo -e "$Y succussfully $2 installed $N"
+fi
+}
+
+for package in $@
+do 
+echo "enter package : $package"
 done
