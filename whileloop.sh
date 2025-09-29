19a3ce8ef91ffb6828b1c73b19173ee30764128a
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-09-29 20:24:00 +0530

    test

--- whileloop.sh
+++ whileloop.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-count=5
+# count=5
 
-while [ $count -ge 0 ]
-do 
-echo "time lest $count"
-sleep 2
-count=$((count-1))
+# while [ $count -ge 0 ]
+# do 
+# echo "time lest $count"
+# sleep 2
+# count=$((count-1))
 
-done 
+# done 
 
-echo "done!"
+# echo "done!"
+
+
+while IFS = read -r line;
+do
+echo "printing"
+done <test.sh
