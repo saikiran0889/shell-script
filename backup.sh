6dbbb5ce9ceb5f80f235fc17234222f6d34510d2
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-09-29 21:37:48 +0530

    test

--- backup.sh
+++ backup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-# ****** Creating log files ********
-logsfolder="/var/log/shell-script"
-scriptname=$( echo $0 | cut -d "." -f1 )
-logfile="$logsfolder/$scriptname.log"
+# USERID=$(id -u)
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+# # ****** Creating log files ********
+# logsfolder="/var/log/shell-script"
+# scriptname=$( echo $0 | cut -d "." -f1 )
+# logfile="$logsfolder/$scriptname.log"
 
-mkdir -p $logsfolder
+# mkdir -p $logsfolder
 
-echo "script started executed : $(date)" | tee -a $logfile
+# echo "script started executed : $(date)" | tee -a $logfile
 
-#********* SUDO access **********
+# #********* SUDO access **********
 
-if [ $USERID -ne 0 ]; then
-echo -e "$R ERROR: You are not in root access $Y"
-exit 1
-fi
+# if [ $USERID -ne 0 ]; then
+# echo -e "$R ERROR: You are not in root access $Y"
+# exit 1
+# fi
 
 # *****************************
 
