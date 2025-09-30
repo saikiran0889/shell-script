8729b3f623d0c64c88fdabe32830f92d4aba35be
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-09-30 13:03:51 +0530

    test

--- cpuusage.sh
+++ cpuusage.sh
@@ -3,12 +3,15 @@
 disk_usage=$(df -hT | grep -v Filesystem)
 disk_treshold=2
 ipaddress=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+message=""
 while IFS= read -r line
 do
 
 usage=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
 partition=$(echo $line | awk '{print $6}')
 if [ $usage -ge $disk_treshold ]; then
-echo "high usage $partiton: $usage"
+message="high disk usage $partition: $usage %"
 fi
 done <<< $disk_usage
+
+echo "message body: $message"
