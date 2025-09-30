80a4303ca050aae7f7208bd9a5f3e9c5d60dbdcf
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-09-30 12:52:50 +0530

    test

--- cpuusage.sh
+++ cpuusage.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 disk_usage=$(df -hT | grep -v Filesystem)
-
+disk_treshold=2
 while IFS= read -r line
 do
 
-usage = echo $line | awk '{print $6}' |
-
+usage=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+partition=$(echo $line | awk '{print $6}')
 done <<< $disk_usage
