4a4e27d174163fb043a31d24060bf96c851fbd02
saikiran0889 <saikiranbuddha1234@gmail.com>
2025-09-30 12:56:30 +0530

    test

--- cpuusage.sh
+++ cpuusage.sh
@@ -7,4 +7,7 @@ do
 
 usage=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
 partition=$(echo $line | awk '{print $6}')
+if [ $usage -ge $disk_treshold ]; then
+echo "high usage $partiton: $usage"
+fi
 done <<< $disk_usage
